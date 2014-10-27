8ab4a6a2d95d328e12e9ff7a27290949f2e28d8a
Ericbui1991 <Ericbui.ntu@gmail.com>
2014-10-28 03:55:03 +0800

    make it better

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ else:
 			'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 			'NAME': "mathsystem",                      # Or path to database file if using sqlite3.
 			'USER': "postgres",                      # Not used with sqlite3.
-			'PASSWORD': '"buivantuong1991",                  # Not used with sqlite3.
+			'PASSWORD': "buivantuong1991",                  # Not used with sqlite3.
 			'HOST': "localhost",                      # Set to empty string for localhost. Not used with sqlite3.
 			'PORT': "5432", 
 		}
